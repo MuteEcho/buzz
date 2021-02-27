8f1cb9d1434beb713bb657bc66c52d6baa6a3644
BCStahley <brookeciera98@gmail.com>
2021-02-27 15:44:19 -0500

    bug fix

---
+++ Assets/scripts/Movebaby.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movebaby : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public float maxUp;
+    public float maxDown;
+    public float maxLeft;
+    public float maxRight;
+
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (rb.transform.position.y > maxUp)
+        {
+            rb.MovePosition(new Vector2(rb.position.x, maxUp));
+        }
+        else if (rb.transform.position.y < maxDown)
+        {
+            rb.MovePosition(new Vector2(rb.position.x, maxDown));
+        }
+        else if (rb.transform.position.x > maxRight)
+        {
+            rb.MovePosition(new Vector2(maxRight, rb.position.y));
+        }
+        else if (rb.transform.position.x < maxLeft)
+        {
+            rb.MovePosition(new Vector2(maxLeft, rb.position.y));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("you died");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
